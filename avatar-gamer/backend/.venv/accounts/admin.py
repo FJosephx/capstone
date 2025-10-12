b292a0f5cc31d79dbb6b9c3dc711fa86ee72c04c
FJosephx <francounda2004@gmail.com>
2025-10-12 14:11:08 -0300

    Limpieza

--- avatar-gamer/backend/.venv/accounts/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import AuthEvent, AccountLock, Profile , OperatorUserLink
-
-admin.site.register(AuthEvent)
-admin.site.register(AccountLock)
-admin.site.register(Profile)
-admin.site.register(OperatorUserLink)
--- avatar-gamer/frontend/avatargamer-app/src/environments/environment.ts
+++ avatar-gamer/frontend/avatargamer-app/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://127.0.0.1:8000/api/v1'  // Reemplazado con tu dirección IP local
+  apiUrl: 'http://localhost:8000/api/v1'  // Reemplazado con tu dirección IP local
 };
