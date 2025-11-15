8530d687854c915e6ddab05e9ec69f89ad2adaf9
FJosephx <francounda2004@gmail.com>
2025-11-15 20:47:07 -0300

    update produccion

--- avatar-gamer/frontend/avatargamer-app/src/environments/environment.prod.ts
+++ avatar-gamer/frontend/avatargamer-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api/v1',  // Reemplazado con tu dirección IP local para pruebas
-  socketUrl: 'http://localhost:8000'  // Para producción real, usa la URL del servidor Socket.IO
+  apiUrl: 'http://18.220.82.0:8000/api/v1',  // Reemplazado con tu dirección IP local para pruebas
+  socketUrl: 'http://18.220.82.0:8000'  // Para producción real, usa la URL del servidor Socket.IO
   // Para producción real, usa la URL de tu servidor de producción
 };
