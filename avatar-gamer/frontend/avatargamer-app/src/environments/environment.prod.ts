b3eecb191b7ac729705371e2eec4e92677b8a0e7
FJosephx <francounda2004@gmail.com>
2025-11-15 21:13:56 -0300

    update produccion v2

--- avatar-gamer/frontend/avatargamer-app/src/environments/environment.prod.ts
+++ avatar-gamer/frontend/avatargamer-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://18.220.82.0/api/v1',  // Reemplazado con tu dirección IP local para pruebas
-  socketUrl: 'http://18.220.82.0'  // Para producción real, usa la URL del servidor Socket.IO
+  apiUrl: '/api/v1',  // Reemplazado con tu dirección IP local para pruebas
+  socketUrl: 'https://capstone-steel-delta.vercel.app'  // Para producción real, usa la URL del servidor Socket.IO
   // Para producción real, usa la URL de tu servidor de producción
 };
